e9356a6e83d8fa545176b86cdfb916084851f82b
Diogo Moura <Diogo Moura>
2016-03-08 01:53:59 +0000

    Added File Splitter

---
+++ src/Chunck.java
@@ -0,0 +1,14 @@
+/**
+ * Created by diogo on 08/03/2016.
+ */
+public class Chunck {
+
+    private int chunckNo;
+    private String fileId;
+
+    Chunck(String fileId,int chunckNo){
+        this.chunckNo=chunckNo;
+        this.fileId=fileId;
+    }
+
+}

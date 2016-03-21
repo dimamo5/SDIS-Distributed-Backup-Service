da59420b3b034fa6790a4cdba05ce7f0dd1f0730
sergio <sergio.mieic@gmail.com>
2016-03-21 15:10:11 +0000

    refactor/rename chunck to chunk

--- src/Chunk.java
+++ src/Chunk.java
@@ -1,13 +1,13 @@
 /**
  * Created by diogo on 08/03/2016.
  */
-public class Chunck {
+public class Chunk {
 
     private int chunckNo;
     private String fileId;
     public static final int MAX_CHUNCK_SIZE=64000;
 
-    Chunck(String fileId,int chunckNo){
+    Chunk(String fileId, int chunckNo){
         this.chunckNo=chunckNo;
         this.fileId=fileId;
     }

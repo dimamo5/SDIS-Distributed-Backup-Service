d81b2e343b43e4e3b32e25dfefad6f0cae547f29
Diogo Moura <Diogo Moura>
2016-03-26 18:17:18 +0000

    Started delete implementation

---
+++ src/protocol/Delete.java
@@ -0,0 +1,14 @@
+package protocol;
+
+/**
+ * Created by diogo on 26/03/2016.
+ */
+public class Delete implements Runnable {
+    public Delete() {
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/protocol/Reclaim.java
@@ -0,0 +1,14 @@
+package protocol;
+
+/**
+ * Created by diogo on 26/03/2016.
+ */
+public class Reclaim implements Runnable {
+    public Reclaim() {
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
